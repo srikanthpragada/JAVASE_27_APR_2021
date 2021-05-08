bc5faef46d376ca425e6144ab88d016072ce7f4e
srikanthpragada <srikanthpragada@gmail.com>
2021-05-08 18:55:28 +0530

    08-MAY

--- demo/src/oop/TestPerson.java
+++ demo/src/oop/TestPerson.java
@@ -7,36 +7,54 @@ class Person {
 		this.name = name;
 		this.email = email;
 	}
+
 	public void print() {
-		System.out.println(this.name);
+		System.out.println(this.name);  // Compile-time polymorphism 
 		System.out.println(this.email);
 	}
-	
+
 	public String getEmail() {
 		return this.email;
 	}
 }
 
 class Employee extends Person {
-    protected String job;
+	protected String job;
+
 	public Employee(String name, String email, String job) {
-		super(name,email);   // Call constructor of superclass 
+		super(name, email); // Call constructor of superclass
 		this.job = job;
 	}
+
 	// Overriding
+	@Override
 	public void print() {
 		super.print();
 		System.out.println(this.job);
 	}
 }
 
-public class TestPerson {
+class Player extends Person {
+	private String game;
 
-	public static void main(String[] args) {
-		Employee e = new Employee("Mike","mike@gmail.com","Programmer");
-		e.print(); 
-		System.out.println(e.getEmail());
-
+	public Player(String name, String email, String game) {
+		super(name, email);
+		this.game = game;
 	}
 
+	@Override
+	public void print() {
+		super.print();
+		System.out.println(this.game);
+	}
+}
+
+public class TestPerson {
+	public static void main(String[] args) {
+		Person p = new Employee("Mike", "mike@gmail.com", "Programmer");
+		p.print();  // Runtime Polymorphism 
+
+		p = new Player("Dhoni", "dhoni@gmail.com", "Cricket");
+		p.print();  // Runtime Polymorphism 
+	}
 }
