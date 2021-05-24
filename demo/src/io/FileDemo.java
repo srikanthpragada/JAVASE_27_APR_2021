1f3e3515e821249c706d02216971c1782db6180a
srikanthpragada <srikanthpragada@gmail.com>
2021-05-24 18:56:32 +0530

    24-MAY

--- demo/src/io/FileDemo.java
+++ demo/src/io/FileDemo.java
@@ -7,9 +7,10 @@ public class FileDemo {
 	public static void main(String[] args) {
 		File f = new File("names.txt");
 		System.out.println(f.exists());
+		f.delete();
 		System.out.println(f.length());
 		
-		File dir = new File("c:\\classroom\\nov30j");
+		File dir = new File("c:\\classroom\\apr27");
 		System.out.println(dir.isDirectory());
 
 	}
--- demo/src/io/NewIODemo.java
+++ demo/src/io/NewIODemo.java
@@ -1,13 +1,18 @@
 package io;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class NewIODemo {
 
-	public static void main(String[] args) {
-		Path path = Paths.get("c:\\classroom\\nov30j");
+	public static void main(String[] args) throws IOException {
+		Path file = Path.of("names.txt");
+		System.out.println(file.getClass());
+		// Files.delete(file);
+		
+		Path path = Path.of("c:\\classroom\\apr27");
 		System.out.println(Files.exists(path));
 
 	}
